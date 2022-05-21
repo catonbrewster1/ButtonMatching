5a8a174c34bf8e983f26b8c4e21efbf15c009f9e
catonbrewster1 <catonbrewster@gmail.com>
2022-05-21 18:05:55 -0500

    grid view of buttons a work in progress. Added xml file for each button...now need to populate the grid

--- app/src/main/java/com/catonbrewster/buttonmatching/MainActivity.kt
+++ app/src/main/java/com/catonbrewster/buttonmatching/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.catonbrewster.buttonmatching
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
